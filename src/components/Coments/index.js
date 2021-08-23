b20e7545685dfd17bb738cc44cf5a5ce6d2ec41b
Natã Sato Rodrigues <slipknotx1xbr@hotmail.com>
2021-08-23 11:24:31 -0300

    Criando rotas, database, modules-23/08/2021

--- src/components/Coments/index.js
+++ src/components/Coments/index.js
@@ -1,18 +1,18 @@
 import { Container } from "./styles";
 
-function Coments() {
+function Coments({coment}) {
     return (
         <Container>
             <header>
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
                 <strong>
-                    por Fulano de tal
+                    por {coment.author.name}
                 </strong>
                 <p>
-                    em 18/08/2021 às 8:23
+                    em {coment.created_at} às 8:23
                 </p>
             </header>
-            <p>Este é o comentário</p>
+            <p>{coment.description}</p>
         </Container>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,30 @@ import Coments from "../../components/Coments";
 import { FeedContainer, GistIcon, Main, Post } from "./styles";
 
 function Home() {
+
+    const posts = [{
+        author: {
+            name: "Fulano",
+        },
+        created_at: "10/10/2021",
+        title: "Este é um post sobre JS",
+        description: "JS é uma linguagem de programação muito top",
+        image: "https://images.ecycle.com.br/wp-content/uploads/2021/06/07193822/lightscape-LtnPejWDSAY-unsplash-2048x1365.jpg.webp",
+        gist: "https://github.com.br/",
+        categories: [
+            "JS", "Back-end", "Express"
+        ],
+        coments: [
+            {
+                author: {
+                    name: "Ciclano",
+                },
+                created_at: "11/10/2021",
+                description: "Realmente JS é muito legal"
+            }
+        ]
+    }];
+
     return (
         <>
             <Header />
@@ -11,9 +35,7 @@ function Home() {
                     Profile
                 </nav>
                 <FeedContainer>
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
+                    {posts.map(post => <PostCard post= {post} />)}
                 </FeedContainer>
                 <aside>
                     Actions
@@ -21,9 +43,9 @@ function Home() {
             </Main>
         </>
     );
-}
+}   
 
-function PostCard() {
+function PostCard({ post }) {
 
     return (
         <Post>
@@ -31,21 +53,22 @@ function PostCard() {
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
                 <div>
                     <strong>
-                        por Fulano de tal
+                        por {post.author.name}
                     </strong>
                     <p>
-                        em 18/08/2021 às 8:23
+                    {post.created_at}
                     </p>
                 </div>
-                <GistIcon />
+                {post.gist && <GistIcon />}
             </header>
             <main>
                 <div>
-                    <h1>Título</h1>
-                    <p>Descrição da postagem bla bla bla</p>
+                    <h1>{post.title}</h1>
+                    <p>{post.description}</p>
                 </div>
-                <img src="https://images.ecycle.com.br/wp-content/uploads/2021/06/07193822/lightscape-LtnPejWDSAY-unsplash-2048x1365.jpg.webp" />
+                <img src={post.image} />
                 <section>
+                    {post.categories.map(category => <p>{category} </p>)}
                     <p>Front-end</p>
                     <p>CSS</p>
                     <p>JS</p>
@@ -53,7 +76,7 @@ function PostCard() {
             </main>
             <footer>
                 <h2>Comentários</h2>
-                <Coments />
+                {post.coments.map(coment => <Coments coment={coment} />)}
             </footer>
         </Post>
 
